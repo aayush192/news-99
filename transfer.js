cd08bda501ad2357373ff0ca2c1a516e2a997b3e
aayush192 <140570881+aayush192@users.noreply.github.com>
2024-08-18 15:47:25 +0545

    Add files via upload

---
+++ transfer.js
@@ -0,0 +1,68 @@
+let search='health';
+let url='';
+const date=new Date();
+
+ async function geturl(search){
+url=`https://newsapi.org/v2/everything?q=${search}&apiKey=270f73a6471e4091befa92f31a39bb2a`;
+
+let content=[];
+    const res= fetch(url).then(res=>res.json())
+    .then((result)=>{
+    content=result.articles.map((val)=>{
+         return val;
+    })
+    console.log(content);
+    let html='';
+    content.forEach(element => {
+        if(element.content!="[Removed]" && element.urlToImage!=null){
+
+      html+=`<div class="box">
+      <div class="item-detail">
+      <div class="img">
+      <div class='img-detail'>
+      <img src='${element.urlToImage}'>
+      </div>
+      </div>
+    <div class="author">${author(element)}</div>
+     <div class="title">${element.title}</div>
+     <div class='content'>${element.content.slice(0,60)}..</div>
+    <div class="detail">${discription(element.description)}..</div>
+    <a href='${element.url}' target='_blank'><button class='More'>more..</button></a>
+    </div></div>`
+        }
+    });
+    document.querySelector('.boxes').innerHTML=html;
+
+})
+ }
+
+geturl(search);
+function discription(element){
+if(element!==null){
+    return element.slice(0,200);
+}
+else{
+    return '';
+}
+}
+function author(element){
+    return`${element.source.name}<br>
+    ${element.publishedAt}`;
+}
+
+const searchButton=document.querySelector('.search-button');
+const searchBar=document.querySelector('.search-bar');
+const option=document.querySelectorAll('.option');
+
+option.forEach(button=>{
+button.addEventListener('click',(event)=>{
+ search=event.target.dataset.nameSearch;
+ geturl(search);
+})
+})
+
+
+searchButton.addEventListener('click',()=>{
+    search=searchBar.value;
+    geturl(search);
+})
